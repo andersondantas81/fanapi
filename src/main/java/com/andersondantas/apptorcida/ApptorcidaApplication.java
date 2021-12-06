ab68ad1afca88e490d019d6dc992e72f86959bce
andersondantas81 <andersondantas81@gmail.com>
2021-12-06 10:56:00 -0300

    first commit

---
+++ src/main/java/com/andersondantas/apptorcida/ApptorcidaApplication.java
@@ -0,0 +1,13 @@
+package com.andersondantas.apptorcida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApptorcidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApptorcidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andersondantas/apptorcida/ApptorcidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andersondantas.apptorcida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApptorcidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
